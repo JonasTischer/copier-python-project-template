ab7703550918808187b268deae1980f78069fd17
Jonas Tischer <jonas.tischer@gmx.de>
2023-01-17 22:09:41 +0100

    Add workflow file to run tests

--- tests/test_within_generated_project.py
+++ tests/test_within_generated_project.py
@@ -1,19 +1,5 @@
-import logging
-from os import environ
-from subprocess import PIPE, Popen
+import pytest
 
-logging.basicConfig(level=logging.INFO)
-
-def test_runs_tests_within_generated_project(project_path):
-    logging.info("Running tests within generated project...")
-    with Popen(
-        ["pytest"],
-        cwd=project_path,
-        stdout=PIPE,
-        bufsize=1,
-        universal_newlines=True,
-    ) as pytest_run:
-        for i in pytest_run.stdout:
-            print(i, end="")
-
-    assert pytest_run.returncode == 0
+    
+def test_within_generated_project(project_path):
+    assert pytest.main([project_path, "-vs"]) == 0
