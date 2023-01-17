f37ee60400cad5dfc06ccc48043248c1dc6444e8
Jonas Tischer <jonas.tischer@gmx.de>
2023-01-17 21:15:45 +0100

    Add subproject and tests

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+from distutils.dir_util import copy_tree
+from os import getenv
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from copier import run_copy
+from git import Repo
+from pytest import fixture
+
+
+class TestAnswers:
+    project_name: str = "Test_project"
+    
+def project_dir(answers: dict):
+    TEMPLATE_PATH = "../"
+    with TemporaryDirectory() as tmp_template_dir:
+        if not getenv("CI"):
+            copy_tree(TEMPLATE_PATH, tmp_template_dir)
+            repo = Repo(tmp_template_dir)
+            repo.git.add(all=True)
+            repo.index.commit("Test template changes")
+
+            src_path = tmp_template_dir
+        else:
+            src_path = str(TEMPLATE_PATH)
+
+        tmp_dir = TemporaryDirectory()
+        run_copy(
+            src_path,
+            tmp_dir.name,
+            answers,
+            vcs_ref="HEAD",
+            exclude=("venv/", ".venv/"),
+            quiet=True,
+        )
+
+    return tmp_dir
+
+
+@fixture(scope="session")
+def project_path():
+    directory = project_dir(TestAnswers())
+
+    yield Path(directory.name)
+    directory.cleanup()
---
+++ tests/test_within_generated_project.py
@@ -0,0 +1,21 @@
+import logging
+from os import environ
+from subprocess import PIPE, Popen
+
+logging.basicConfig(level=logging.INFO)
+
+
+def test_runs_tests_within_generated_project(project_path):
+    logging.info("Running tests within generated project...")
+    with Popen(
+        ["python3", "-m", "pytest"],
+        env=dict(environ),
+        cwd=project_path,
+        stdout=PIPE,
+        bufsize=1,
+        universal_newlines=True,
+    ) as pytest_run:
+        for i in pytest_run.stdout:
+            print(i, end="")
+
+    assert pytest_run.returncode == 0
